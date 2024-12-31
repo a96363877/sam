b3204464e6e53401fd60866e28aa014f7cfc73e8
a96363877 <a96363877@gmail.com>
2024-12-31 03:40:00 -0800

    tf

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
   const { state } = useCart()
   const router = useRouter()
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-[#005B8E] text-white p-4 m-2 rounded-full mt-5">
+    <div className="fixed bottom-0 left-0 right-0 bg-[#005B8E] text-white p-2 m-2 rounded-full mt-5">
       <div className="container mx-auto flex items-center justify-between">
       <Button variant={'ghost'} className=" p-2 rounded-full" onClick={()=>router.push('/cart')}>
       <ChevronLeft className="h-6 w-6" />
